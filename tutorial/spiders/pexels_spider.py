296e71926727af81639dc41a4346077cb32875a5
Ayush Gupta <ayushgp10@gmail.com>
2017-04-25 22:25:56 +0530

    Modify functions for checking existing code and update img_id extraction

--- tutorial/spiders/pexels_spider.py
+++ tutorial/spiders/pexels_spider.py
@@ -6,6 +6,7 @@ from scrapy.selector import Selector
 
 from ..DBUtil import DBUtil
 
+
 class PexelsScraper(scrapy.Spider):
     name = "pexels"
     start_urls = ["https://www.pexels.com/"]
@@ -30,13 +31,14 @@ class PexelsScraper(scrapy.Spider):
             images = body.css('img.image-section__image').extract()
             for image in images:
                 img_url = PexelsScraper.src_extractor.findall(image)[0]
+                img_id = self.get_image_id(response.url)
                 tags = [tag.replace(',', '') for tag in PexelsScraper.tags_extractor.findall(image)[0].split(' ')]
-                PexelsScraper.db_util.create_index(self.get_image_id(response.url),
+
+                PexelsScraper.db_util.create_index(img_id,
                                                    1,
                                                    img_url,
                                                    response.url,
                                                    tags)
-                print tags
 
             link_extractor = LinkExtractor(allow=PexelsScraper.url_matcher)
             next_links = filter(self.check_if_extracted,
@@ -49,10 +51,9 @@ class PexelsScraper(scrapy.Spider):
     Works based off of id for this particular website. 
     """
     def check_if_extracted(self, img_url):
-        # Image urls are of type: https://www.pexels.com/photo/asphalt-blur-clouds-dawn-392010/
-        # img_id = img_url.split('/')[-2].split('-')[-1]
-        return True
+        return not PexelsScraper.db_util.check_if_image_exists(1, self.get_image_id(img_url))
 
     def get_image_id(self, url):
+        # Image urls are of type: https://www.pexels.com/photo/asphalt-blur-clouds-dawn-392010/
         return url.split('/')[-2].split('-')[-1]
 
