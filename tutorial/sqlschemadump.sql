0f59e8d7f6363c7e2bf21ac2ec367fc10e91bb29
Ayush Gupta <ayushgp10@gmail.com>
2017-04-25 12:02:33 +0530

    Add database schema file

---
+++ tutorial/sqlschemadump.sql
@@ -0,0 +1,27 @@
+CREATE TABLE websites(
+    web_id      INT(5) NOT NULL,
+    webname VARCHAR(20) NOT NULL,
+    url     VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE images(
+    img_id          INT(20) NOT NULL,
+    url         VARCHAR(2083) NOT NULL,
+    page_url    VARCHAR(2083) NOT NULL,
+    PRIMARY KEY(img_id)
+);
+
+CREATE TABLE tags(
+    tag_id          INT(10) NOT NULL,
+    tag_name    VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE imgtags(
+    img_id      INT(20) NOT NULL,
+    tag_id      INT(10) NOT NULL,
+    weight      INT(10),
+    FOREIGN KEY(img_id) REFERENCES images(img_id),
+    FOREIGN KEY(tag_id) REFERENCES tags(tag_id)
+);
