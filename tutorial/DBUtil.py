f3082ccf3807614617413c0725fd03779e27ca6d
Ayush Gupta <ayushgp10@gmail.com>
2017-04-25 12:38:47 +0530

    Add query for inserting tags as well.

--- tutorial/DBUtil.py
+++ tutorial/DBUtil.py
@@ -11,9 +11,19 @@ class DBUtil(object):
         try:
             with self.connection.cursor() as cursor:
                 img_insert_stmt = "INSERT INTO `images` (`img_id`, `url`, `page_url`) VALUES(%s, %s, %s)"
-                # tags_insert_stmt = "INSERT INTO `imgtags`(`img_id`, `tag_id`) VALUES(%s, %s)"
+                img_tags_rel_stmt = "INSERT INTO `imgtags`(`img_id`, `tag_id`) VALUES" + \
+                                    ",".join("(%s, %s)" for tag in tags)
+
                 cursor.execute(img_insert_stmt, (img_id, img_url, page_url))
-                # cursor.execute(tags_insert_stmt)
+
+                # Construct the insert query for tagging
+                num_tags = len(tags)
+                tag_list = [0] * (num_tags * 2)
+                for i in xrange(num_tags):
+                    tag_list[2 * i] = img_id
+                    tag_list[2 * i + 1] = tags[i]
+                cursor.execute(img_tags_rel_stmt, tag_list)
+
                 self.connection.commit()
 
         finally:
